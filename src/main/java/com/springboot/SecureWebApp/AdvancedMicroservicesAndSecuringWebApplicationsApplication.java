408b56ab1cf52fb9adc0a900e7b99a84ea20fd68
Pierre <Philben.Pierre@pepsico.com>
2024-08-01 11:57:41 -0500

    Initial commit

---
+++ src/main/java/com/springboot/SecureWebApp/AdvancedMicroservicesAndSecuringWebApplicationsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.SecureWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedMicroservicesAndSecuringWebApplicationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvancedMicroservicesAndSecuringWebApplicationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/SecureWebApp/AdvancedMicroservicesAndSecuringWebApplicationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.SecureWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedMicroservicesAndSecuringWebApplicationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
